47c2c3f479e6eb75dae3704c0c2409e103c2fdb7
rajat404 <404rajat@gmail.com>
2015-08-26 02:57:48 +0530

    APIs completed

--- src/main/app.py
+++ src/main/app.py
@@ -9,19 +9,27 @@ parser.read('config.ini')
 
 api_key = parser.get("settings", "api_key")
 group_name = parser.get("settings", "group_name")
-event_id = parser.get("settings", "event_id")
-
-url = "https://api.meetup.com/{}/events/{}/attendance?key={}".format(group_name,event_id,api_key)
 
 app = Flask(__name__)
 
 class AttendanceView(FlaskView):
+
     def index(self):
         return "Hello World"
 
 
-    @route('/complete/')
-    def get_roster(self):
+    @route('/<event_id>/complete/')
+    def get_roster(self, event_id):
+        """
+        Description: Get list of all the people who RSVP'd `yes`
+        Params:
+            - event_id: unique number repersenting a single event (found in the URL)
+
+        URL: <Host-IP>/attendance/<event_id>/complete/ 
+        Sample Request: <Host-IP>/attendance/224668752/complete/ 
+        """
+        event_id = int(event_id)
+        url = "https://api.meetup.com/{}/events/{}/attendance?key={}".format(group_name,event_id,api_key)
         req = requests.get(url)
         response = req.json()
         # List of all the attendees
@@ -31,6 +39,26 @@ class AttendanceView(FlaskView):
             temp[attendee['member']['id']] = str(attendee['member']['name']).lower()
             all_attendees.append(temp)
         return json.dumps(all_attendees), 200
+
+
+    @route('/<event_id>/mark/<member_id>/<member_status>/', endpoint='mark_attendance')
+    def mark_attendance(self, event_id, member_id, member_status):
+        """
+        Description: Marks the attendance of an individual.
+        Params:
+            - event_id: unique number repersenting a single event (found in the URL)
+            - member_id: ID of each member of the group, can be fetched by /complete/ API
+            - member_status: can be `absent` or `attended` or `noshow`
+
+        URL: <Host-IP>/attendance/<event_id>/mark/<member_id>/<member_status>/ 
+        Sample Request: <Host-IP>/attendance/224668752/mark/58178002/absent/ 
+        """
+        event_id = int(event_id)
+        payload = {'member':member_id, 'status':member_status}
+        url = "https://api.meetup.com/{}/events/{}/attendance?key={}".format(group_name,event_id,api_key)
+        req = requests.post(url, params=payload)
+        response = req.json()
+        return json.dumps(response), 200
 
 
 AttendanceView.register(app)
