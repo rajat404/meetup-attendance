7297bf3be15ae268d5c2a76e1fd096d404404c8f
rajat404 <404rajat@gmail.com>
2015-09-27 23:11:29 +0530

    fixed some minor stuff, might need some additional work.

--- meetup-attendance/angular/js/controllers.js
+++ meetup-attendance/angular/js/controllers.js
@@ -2,13 +2,15 @@ angular.module('mainController', [])
     
     .controller('postController', ['$scope','$http', '$routeParams', 'postService', function($scope, $http, $routeParams, postService) {
         $scope.event_id = $routeParams.event_id;
-
+        window.console.log("$scope.event_id:",$scope.event_id);
         $scope.$on('$viewContentLoaded', function(){
             $scope.home();
         });
 
         $scope.home = function(){
             postService.getRoster($scope.event_id).success(function(data) {
+                window.console.log("$scope.event_id2:",$scope.event_id);
+
                 $scope.response = data;
                 window.console.log("$scope.response:",$scope.response);
                 if ($scope.response != null){
