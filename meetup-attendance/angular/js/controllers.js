610ca358dd862dc74b604353c55a163876f98cd6
rajat404 <404rajat@gmail.com>
2015-09-28 00:09:39 +0530

    Future self, please forgive me and don't hit me with the baseball bat again!

--- meetup-attendance/angular/js/controllers.js
+++ meetup-attendance/angular/js/controllers.js
@@ -2,22 +2,33 @@ angular.module('mainController', [])
     
     .controller('postController', ['$scope','$http', '$routeParams', 'postService', function($scope, $http, $routeParams, postService) {
         $scope.event_id = $routeParams.event_id;
-        window.console.log("$scope.event_id:",$scope.event_id);
         $scope.$on('$viewContentLoaded', function(){
             $scope.home();
         });
 
         $scope.home = function(){
             postService.getRoster($scope.event_id).success(function(data) {
-                window.console.log("$scope.event_id2:",$scope.event_id);
 
                 $scope.response = data;
                 window.console.log("$scope.response:",$scope.response);
-                if ($scope.response != null){
-                }
-                else{
-                    alert("No Data Available");
-                }
+                // if ($scope.response != null){
+                // }
+                // else{
+                //     alert("No Data Available");
+                // }
+                    $(document).ready(function(){
+
+        $('input.typeahead').typeahead({
+
+          name: 'accounts',
+
+          local: ['Audi', 'BMW', 'Bugatti', 'Ferrari', 'Ford', 'Lamborghini', 'Mercedes Benz', 'Porsche', 'Rolls-Royce', 'Volkswagen']
+
+        });
+
+    });
+
+                    
             });
         }
 
