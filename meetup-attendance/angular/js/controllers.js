f8d590fd84767e1da7c98926a877b01585b7bf02
rajat404 <404rajat@gmail.com>
2015-09-28 00:34:15 +0530

    changes

--- meetup-attendance/angular/js/controllers.js
+++ meetup-attendance/angular/js/controllers.js
@@ -6,6 +6,9 @@ angular.module('mainController', [])
             $scope.home();
         });
 
+  $scope.selected = undefined;
+  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
         $scope.home = function(){
             postService.getRoster($scope.event_id).success(function(data) {
 
